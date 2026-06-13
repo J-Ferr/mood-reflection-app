2ba80ff712e11a81aa24d5fbb24bcbdc74ace327
Jose Armando <Arellano.53j@gmail.com>
2026-06-13 00:48:53 -0400

    Add password reset flo with email support

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,6 +5,7 @@ const crypto = require("crypto");
 const pool = require("../db/pool");
 const { Pool } = require("pg");
 const bcrypt = require("bcrypt");
+const sendEmail = require("../utils/sendEmail");
 
 router.post("/register", register);
 router.post("/login", login);
@@ -44,7 +45,17 @@ router.post("/forgot-password", async (req, res, next) => {
 
     const resetLink = `http://localhost:5173/reset-password/${resetToken}`;
 
-    console.log("Reset link:", resetLink);
+    await sendEmail({
+      to: email,
+      subject: "Reset your Mood Reflection password",
+      html: `
+        <h2>Password Reset</h2>
+        <p>You requested to reset your password.</p>
+        <p>Click the link below:</p>
+        <a href="${resetLink}">${resetLink}</a>
+        <p>This link will expire soon.</p>
+      `,
+    });
 
     return res.json({
       message: "If an account exists with that email, a reset link will be sent.",
