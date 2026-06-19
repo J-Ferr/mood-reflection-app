04f2d7aeb1eb005d756852456b90336afc723c1d
Jose Armando <Arellano.53j@gmail.com>
2026-06-19 18:39:52 -0400

    Switch reset password emails to Resend

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,39 +1,21 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-  console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    logger: true,
-    debug: true,
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
+  const { data, error } = await resend.emails.send({
+    from: "Mood Reflection App <onboarding@resend.dev>",
+    to,
+    subject,
+    html,
   });
 
-  try {
-    console.log("Attempting to send email...");
-
-    const info = await transporter.sendMail({
-      from: `"Mood Reflection App" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("EMAIL SENT:", info.messageId);
-  } catch (error) {
-    console.error("EMAIL ERROR NAME:", error.name);
-    console.error("EMAIL ERROR CODE:", error.code);
-    console.error("EMAIL ERROR MESSAGE:", error.message);
+  if (error) {
+    console.error("RESEND ERROR:", error);
     throw error;
   }
+
+  console.log("RESEND EMAIL SENT:", data);
 };
 
 module.exports = sendEmail;
